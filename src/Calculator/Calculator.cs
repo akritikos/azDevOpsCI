f5b0c6c2adaa14a4fb3d78b73f7b9cec7a6575ee
Alexandros Kritikos <akritikos@outlook.com>
2020-11-21 18:16:51 +0200

    Adds a subtraction method

--- src/Calculator/Calculator.cs
+++ src/Calculator/Calculator.cs
@@ -7,5 +7,7 @@ namespace Kritikos.Calculator
 		internal static Random Random { get; } = new Random();
 
 		public static int Add(int x, int y) => x + y;
+
+		public static int Subtract(int x, int y) => x - y;
 	}
 }
