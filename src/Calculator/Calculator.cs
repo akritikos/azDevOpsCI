224eb817acc74873a082dab8ca12201d8e875bf0
Alexandros Kritikos <akritikos@outlook.com>
2021-03-05 19:09:05 +0200

    Fixes step placement

--- src/Calculator/Calculator.cs
+++ src/Calculator/Calculator.cs
@@ -9,5 +9,17 @@ namespace Kritikos.Calculator
 		public static int Add(int x, int y) => x + y;
 
 		public static int Subtract(int x, int y) => x - y;
+
+		public static bool IsAdult(this Person person)
+			=> person?.Age >= 18;
+
+		static void Foo()
+		{
+			var p = new Person("Alexandros", 20);
+		}
+	}
+
+	public record Person(string Name, int Age)
+	{
 	}
 }
