a9f0c06698a4419e8c706110b56cb15930dae93f
Alexandros Kritikos <akritikos@outlook.com>
2021-04-22 16:18:39 +0300

    Removes XPlat coverage

--- src/Calculator/Calculator.cs
+++ src/Calculator/Calculator.cs
@@ -1,25 +1,20 @@
 namespace Kritikos.Calculator
 {
-	using System;
+  using System;
 
-	public static class Calculator
-	{
-		internal static Random Random { get; } = new Random();
+  public static class Calculator
+  {
+    internal static Random Random { get; } = new Random();
 
-		public static int Add(int x, int y) => x + y;
+    public static int Add(int x, int y) => x + y;
 
-		public static int Subtract(int x, int y) => x - y;
+    public static int Subtract(int x, int y) => x - y;
 
-		public static bool IsAdult(this Person person)
-			=> person?.Age >= 18;
+    public static bool IsAdult(this Person person)
+      => person?.Age >= 18;
+  }
 
-		static void Foo()
-		{
-			var p = new Person("Alexandros", 20);
-		}
-	}
-
-	public record Person(string Name, int Age)
-	{
-	}
+  public record Person(string Name, int Age)
+  {
+  }
 }
